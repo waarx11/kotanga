16bfee655a1c91ad33ea6768a82efd81436d28e2
Nathan <nathanverdier34@gmail.com>
2023-03-22 00:38:39 -0400

    Base json ajouter et foncionnel relier à firebase

---
+++ Kotanga/app/src/main/java/com/example/kotanga/Kotanga.kt
@@ -0,0 +1,31 @@
+package com.example.kotanga
+
+import android.app.Application
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+class Kotanga : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        val database = FirebaseDatabase.getInstance().reference
+        database.child("message").setValue("Bonjour, Firebase !")
+
+        /*database.child("message").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val message = dataSnapshot.getValue(String::class.java)
+                Log.d("Firebase", "Message : $message")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("Firebase", "Erreur : " + databaseError.message)
+            }
+        })*/
+
+    }
+}
