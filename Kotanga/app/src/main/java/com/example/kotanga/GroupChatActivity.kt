587752cd5e3a53ef029bf20250e7c0b710808064
Simon Claus <simon.claus@laposte.net>
2023-04-28 15:21:50 -0400

    message

--- Kotanga/app/src/main/java/com/example/kotanga/GroupChatActivity.kt
+++ Kotanga/app/src/main/java/com/example/kotanga/GroupChatActivity.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import com.example.kotanga.databinding.ActivityGroupChatBinding
-import com.example.kotanga.databinding.ActivityHomeBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -21,6 +20,7 @@ class GroupChatActivity : AppCompatActivity() {
     private lateinit var groupLayout: LinearLayout
     private lateinit var currentUser: DatabaseReference
     private lateinit var groupName: String
+    private lateinit var messageAdapter: ArrayAdapter<String>
 
     private lateinit var binding: ActivityGroupChatBinding
     private lateinit var dbManager: FirebaseManager
@@ -32,6 +32,9 @@ class GroupChatActivity : AppCompatActivity() {
         setContentView(binding.root)
         val groupNameTop = intent.getStringExtra("groupName")
 
+
+        val messageEditText: EditText = findViewById(R.id.messageEditText)
+
         val database = Firebase.database
         val userId = Firebase.auth.currentUser?.uid
         currentUser = database.getReference("users/$userId")
@@ -39,9 +42,9 @@ class GroupChatActivity : AppCompatActivity() {
             intent.getStringExtra("groupName").toString() // Récupération du nom du groupe depuis l'intent
 
         addUserInGroup = findViewById(R.id.middle_button) // Ajout de l'ID du bouton
-        groupLayout = findViewById(R.id.group_layout) // Ajout de l'ID de la vue parente
+        //groupLayout = findViewById(R.id.group_layout) // Ajout de l'ID de la vue parente
 
-        addUserInGroup.setOnClickListener {
+        /*addUserInGroup.setOnClickListener {
             val builder = AlertDialog.Builder(this)
             val view = LayoutInflater.from(this).inflate(R.layout.popup_add_user_to_group, null)
             builder.setView(view)
@@ -104,8 +107,7 @@ class GroupChatActivity : AppCompatActivity() {
 
             val dialog = builder.create()
             dialog.show()
-        }
-
+        }*/
 
         binding.groupName.text = "$groupNameTop"
 
@@ -124,5 +126,60 @@ class GroupChatActivity : AppCompatActivity() {
         binding.parambutton.setOnClickListener{
             startActivity(Intent(this, ParametersActivity::class.java))
         }
+
+        messageAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        binding.messageList.adapter = messageAdapter
+
+        val messageRef = database.getReference("groupes/$groupName/messages")
+        messageRef.orderByChild("timestamp").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (childSnapshot in dataSnapshot.children) {
+                    val message = childSnapshot.getValue(Message::class.java)
+                    if (message != null) {
+                        val messageText = "${message.author}: ${message.content}"
+                        messageAdapter.add(messageText)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+        binding.sendMessageButton.setOnClickListener {
+            val messageContent = binding.messageEditText.text.toString()
+            val userId = Firebase.auth.currentUser?.uid
+            val userRef = database.getReference("users/$userId/name")
+            userRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val name = dataSnapshot.getValue(String::class.java)
+                    val firstName = name?.substringBefore(" ")
+                    val lastName = name?.substringAfter(" ")
+                    val nomVal = lastName?.substringBefore(" ")
+                    val authorName = firstName + " " + nomVal
+                    writeMessage(groupName, messageContent, authorName)
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Gérer l'erreur ici
+                }
+            })
+
+            binding.messageEditText.text.clear()
+        }
+        }
+
+
     }
-}
+    data class Message(
+        val author: String = "",
+        val content: String = "",
+        val timestamp: Long = 0
+    )
+    fun writeMessage(groupName: String, messageContent: String, authorName: String) {
+        val database = Firebase.database
+        val groupRef = database.reference.child("groupes").child(groupName)
+        val messageRef = groupRef.child("messages").push()
+
+        val message = Message(authorName, messageContent, System.currentTimeMillis())
+        messageRef.setValue(message)
+    }
